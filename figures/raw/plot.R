ce09d5d588061979be89e575ab8a336cf37fc6c6
Yannick Lang <layaxx@gmx.de>
2024-11-03 11:00:32 +0100

    add sections wardriving, rssi and draft of trilateration

---
+++ figures/raw/plot.R
@@ -0,0 +1,21 @@
+library(ggplot2)
+library(tidyr)
+
+df <- data.frame(distance=            1:15,
+                 wigle=           c(-37,-53,-52,-54,-56,-63,-56,-65,-65,-55,-64,-69,-66,-64,-61),
+                 wifidata=        c(-44,-54,-56,-59,-58,-53,-59,-54,-63,-68,-63,-66,-61,-65,-69),
+                 signalstrength=  c(-43,-48,-50,-50,-54,-57,-59,-55,-60,-62,-61,-63,-65,-66,-64)
+                 )
+df <- df %>% pivot_longer(cols=c('wigle', 'wifidata', 'signalstrength'),
+                          names_to='app',
+                          values_to='signal')
+
+
+lbs <- c("wigle" = "WiGLE WiFi", "wifidata" = "WiFi Data", "signalstrength" = "Signal Strength")
+
+ggplot(data=df, aes(x=distance, y=signal, group=app)) +
+  geom_line(linetype = "dashed", aes(color=app))+
+  geom_point(aes(color=app))+
+  #facet_wrap(~app, ncol=1, labeller=as_labeller(lbs))+
+#  theme(legend.position="none")
+
